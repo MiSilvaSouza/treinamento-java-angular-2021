0ba3f70ff24d97acfc5da8a5eb7c698ae97f9e2a
MiSilvaSouza <mirian.s2@hotmail.com>
2021-11-20 14:09:29 -0300

    Conexão e consulta no banco de dados

--- src/main/java/com/company/app/dao/OrderDao.java
+++ src/main/java/com/company/app/dao/OrderDao.java
@@ -1,38 +1,81 @@
 package com.company.app.dao;
 
+import com.company.app.model.ClientModel;
 import com.company.app.model.OrderModel;
+import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Date;
+import java.util.Optional;
 import java.util.Properties;
 
+@Slf4j
 public class OrderDao {
 
     private Connection connection;
 
     public OrderDao() throws SQLException{
         connection = createConnection();
+        log.info("Conexão executada com sucesso!");
     }
 
     private Connection createConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost/test";
+        String url = "jdbc:postgresql://localhost/postgres";
         Properties props = new Properties();
-        props.setProperty("user","fred");
-        props.setProperty("password","secret");
-        props.setProperty("ssl","true");
+        props.setProperty("user","postgres");
+        props.setProperty("password","postgres");
+        //props.setProperty("ssl","true");
         return DriverManager.getConnection(url, props);
     }
 
     public OrderModel insert(OrderModel order) {
+
         return order;
     }
 
     public OrderModel update(OrderModel order) {
+
         return order;
     }
 
-    public OrderModel searchByCode(Integer code) {
-        return null;
+    public Optional <OrderModel> searchByCode(Integer idQuery) throws SQLException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("select code ");
+        sb.append("     , client_code ");
+        sb.append("     , amount ");
+        sb.append("     , cpf ");
+        sb.append("     , date ");
+        sb.append(" from orders ");
+        sb.append(" where code = " + idQuery);
+
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery(sb.toString());
+
+        if (!rs.next()) {
+            return Optional.empty();
+        }
+
+        int i = 1;
+        Integer code = rs.getInt(i++);
+        Integer client_code = rs.getInt(i++);
+        BigDecimal amount = rs.getBigDecimal(i++);
+        String cpf = rs.getString(i++);
+        Date date = rs.getDate(i);
+
+        ClientModel client = ClientModel.builder()
+                .code(client_code)
+                .build();
+
+        OrderModel order = OrderModel.builder()
+                .code(code)
+                .client(client)
+                .amount(amount)
+                .cpf(cpf)
+                .date(date)
+                .build();
+
+        return Optional.of(order);
     }
 }
--- src/main/java/com/company/app/services/ServiceProcessFiles.java
+++ src/main/java/com/company/app/services/ServiceProcessFiles.java
@@ -1,17 +1,22 @@
 package com.company.app.services;
 
 import com.company.app.Program;
+import com.company.app.dao.OrderDao;
 import com.company.app.model.OrderModel;
 import com.company.app.validate.ValidateFiles;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.swing.plaf.synth.SynthRadioButtonMenuItemUI;
 import java.io.File;
 import java.nio.file.Path;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 public class ServiceProcessFiles {
 
+    private final OrderDao orderDao;
     private final Path dirEntrada;
     private final List<String> listFiles;
 
@@ -19,6 +24,14 @@ public class ServiceProcessFiles {
         ValidateFiles validate = new ValidateFiles();
         dirEntrada = validate.validateDirEntrada(caminhoDirEntrada);
         listFiles = validate.getListFiles();
+        try {
+            orderDao = new OrderDao();
+        } catch (SQLException e) {
+            String msg = "Ocorreu um erro ao criar DAO de pedidos";
+            log.error(msg, e);
+            throw new RuntimeException(msg, e);
+
+        }
     }
 
     public void executeService() {
@@ -43,5 +56,13 @@ public class ServiceProcessFiles {
 
             //TODO - gravar no banco de dados
         });
+
+        try {
+            Optional<OrderModel> printOrder = orderDao.searchByCode(7126);
+            System.out.println(printOrder);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
